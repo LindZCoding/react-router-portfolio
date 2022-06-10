5019787c3bade773f51764c6bf6ddaf8eefdb50b
lindzcoding <lindzgaming@gmail.com>
2022-06-09 19:46:50 -0700

    edited portfolio

--- portfolio-react/src/components/pages/About.js
+++ portfolio-react/src/components/pages/About.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 
 export default class About extends Component {
-    render () {
+    render() {
         return (
-        <>
-            <h1>About me!🌸</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida cum sociis natoque penatibus et. Et sollicitudin ac orci phasellus egestas tellus rutrum tellus. Pulvinar mattis nunc sed blandit libero volutpat. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Et ligula ullamcorper malesuada proin libero. Sit amet justo donec enim diam vulputate ut. Nibh tortor id aliquet lectus proin nibh nisl condimentum. Neque viverra justo nec ultrices dui sapien eget. Leo urna molestie at elementum eu facilisis. Tristique senectus et netus et malesuada. Fames ac turpis egestas sed tempus urna et pharetra. Neque viverra justo nec ultrices dui sapien eget mi proin.</p>
-        </>
+            <div className="about">
+                <h1>About me!🌸</h1>
+                <h4>Email Me</h4>
+                <p>Lindsaytaylor925@gmail.com</p>
+                {/* <br />
+                <a href="/Lindsay_Haigh_Resume.pdf" target='_blank'><button>RESUME</button></a> */}
+            </div>
         )
     }
 
--- portfolio-react/src/components/partials/Header.js
+++ portfolio-react/src/components/partials/Header.js
@@ -8,10 +8,11 @@ export default class Header extends Component {
             <header>
                 <div className='links' className='active'>
                 <nav>
+                <h1 className="header-name">Lindsay Haigh🌸</h1>
                     <ul>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/blog'>Blog</Link></li>
-                        <li><Link to='/about'>About</Link></li>
+                        <li><a href="https://github.com/LindZCoding">GitHub</a></li>
+                        <li><a href="https://www.linkedin.com/in/lindsaytaylorhaigh/">LinkedIn</a></li>
+                        <li><a href="/Lindsay_Haigh_Resume.pdf" target='_blank'>Resume</a></li>
                         <li><Link to='/projects'>Projects</Link></li>
                     </ul>
                 </nav>
